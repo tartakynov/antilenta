f859ef4d98c8dda6d58bf93f09fb01f9585470dc
Artem Tartakynov <artem.tartakynov@agoda.com>
2017-01-21 19:22:30 +0700

    added redirect from lifenews to buzzfeed

--- background.js
+++ background.js
@@ -1,7 +1,21 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
+var callback = function(details) {
+  var lenta = /^[\w]+:\/\/lenta.ru.*$/i;
+  var life = /^[\w]+:\/\/life(news){0,1}.ru.*$/i;
+  if (details.url.match(lenta)) {
     return {redirectUrl: "https://meduza.io/"};
-  },
-  {urls: ["*://lenta.ru/*"]}, // list of URLs
-  ["blocking"] // Block intercepted requests until this handler has finished
-);
+  } else if (details.url.match(life)) {
+    return {redirectUrl: "https://buzzfeed.com/"};
+  }
+};
+
+var filter = {
+  urls: [
+    "*://lenta.ru/*",
+    "*://life.ru/*",
+    "*://lifenews.ru/*"
+  ]
+};
+
+var opt_extraInfoSpec = ["blocking"];
+
+chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
